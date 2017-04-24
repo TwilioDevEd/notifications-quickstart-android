d9cf91f755717d1333ec879050974b669eaa9e41
Viktor Muller <vmuller@twilio.com>
2017-04-24 15:47:43 +0100

    Reverted server host constant to placeholder.

--- app/src/main/java/com/twilio/notify/quickstart/notifyapi/TwilioSDKStarterAPI.java
+++ app/src/main/java/com/twilio/notify/quickstart/notifyapi/TwilioSDKStarterAPI.java
@@ -12,7 +12,7 @@ import retrofit2.http.Body;
 import retrofit2.http.POST;
 
 public class TwilioSDKStarterAPI {
-    public final static String BASE_SERVER_URL = "https://f107bbc9.ngrok.io";
+    public final static String BASE_SERVER_URL = "YOUR_SDK_STARTER_SERVER_URL";
 
     /**
      * A resource defined to register Notify bindings using the sdk-starter projects available in
